e6990512a8621ed3f8cf7ca28b82cb75b00b4f6d
joelbrownstein <joelbrownstein@AlbireoAir.local>
2015-08-20 22:57:06 +0000

    sdss4tools

--- bin/install_tree.sh
+++ bin/install_tree.sh
@@ -10,27 +10,6 @@ function print_and_run {
     echo "$@"
     eval "$@"
 }
-#
-# Check for INSTALL_DIR
-#
-if [ -z "${INSTALL_DIR}" ]; then
-    echo "INSTALL_DIR is not defined, skipping this step."
-    exit 0
-fi
-#
-# Install version files
-#
-installbase=$(dirname ${INSTALL_DIR})
-for table in *.table; do
-    version=$(echo ${table} | cut -d. -f1)
-    print_and_run mkdir -p ${installbase}/${version}/bin
-    print_and_run cp -pf ${version}_version ${installbase}/${version}/bin/tree_version
-    print_and_run chmod +x ${installbase}/${version}/bin/tree_version
-done
-#
-# Install Module files
-#
-installbase=$(dirname ${installbase})
 treemodules=''
 for m in $(echo ${MODULEPATH} | tr ':' ' '); do
     if [ -d ${m}/tree ]; then
@@ -45,7 +24,7 @@ if [ -w "${treemodules}" ]; then
     print_and_run mkdir -p ${treemodules}/tree
     for module in *.module; do
         version=$(echo ${module} | cut -d. -f1)
-        print_and_run "cat ${module} | sed \"s%@INSTALL_DIR@%${installbase}%\" > ${treemodules}/tree/${version}"
+        print_and_run cp -pf ${module} ${treemodules}/tree/${version}
     done
     print_and_run cp -pf .version ${treemodules}/tree
 else
