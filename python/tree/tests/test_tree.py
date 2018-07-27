be819afdcb171cd452c7660a0b895aa5e28e6ae0
Brian Cherinka <havok2063@hotmail.com>
2018-07-27 17:34:08 -0400

    updating some tests to include manga_spectro_analysis and dr15

--- python/tree/tests/test_tree.py
+++ python/tree/tests/test_tree.py
@@ -6,7 +6,7 @@
 # @Author: Brian Cherinka
 # @Date:   2017-11-29 10:28:58
 # @Last modified by:   Brian Cherinka
-# @Last Modified time: 2018-03-08 10:58:37
+# @Last Modified time: 2018-07-27 17:22:02
 
 from __future__ import print_function, division, absolute_import
 import pytest
@@ -42,26 +42,33 @@ class TestTree(object):
         for key in keys:
             assert key.upper() in t.environ
 
-    @pytest.mark.parametrize('dr', [('dr14')])
+    @pytest.mark.parametrize('dr', [('dr15')])
     def test_load_dr_config(self, dr):
         t = Tree(config=dr)
         assert dr == t.environ['default']['name']
 
-    @pytest.mark.parametrize('dr', [('dr14')])
+    def _assert_paths(self, tree, dr=None):
+        assert 'manga_spectro_redux' in tree.environ['MANGA']
+        assert 'manga_spectro_analysis' in tree.environ['MANGA']
+        if dr:
+            assert dr in tree.environ['default']['name']
+            assert dr in tree.environ['MANGA']['manga_root']
+            assert dr in tree.environ['MANGA']['manga_spectro_redux']
+            assert dr in tree.environ['MANGA']['manga_spectro_analysis']
+
+    @pytest.mark.parametrize('dr', [('dr15')])
     def test_load_with_update(self, tree, dr):
         assert 'sdsswork' in tree.environ['default']['name']
         assert 'mangawork' in tree.environ['MANGA']['manga_root']
         tree = Tree(config=dr, update=True)
-        assert dr in tree.environ['default']['name']
-        assert dr in tree.environ['MANGA']['manga_root']
+        self._assert_paths(tree, dr=dr)
 
-    @pytest.mark.parametrize('dr', [('dr14')])
+    @pytest.mark.parametrize('dr', [('dr15')])
     def test_replant_tree(self, tree, dr):
         assert 'sdsswork' in tree.environ['default']['name']
         assert 'mangawork' in tree.environ['MANGA']['manga_root']
         tree.replant_tree(dr)
-        assert dr in tree.environ['default']['name']
-        assert dr in tree.environ['MANGA']['manga_root']
+        self._assert_paths(tree, dr=dr)
 
     def test_custom_paths_remain(self, monkeypatch):
         # set custom environ variable
