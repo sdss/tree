22a674c083ea8d41b49f5c9f9851deb07b0721d9
Brian Cherinka <havok2063@hotmail.com>
2020-04-13 15:29:54 -0400

    Bump version: 2.15.10dev → 2.15.10

--- python/tree/__init__.py
+++ python/tree/__init__.py
@@ -26,4 +26,4 @@ with open(os.path.dirname(__file__) + '/etc/{0}.cfg'.format(NAME)) as ff:
         config = yaml.load(ff)
 
 
-__version__ = '2.15.10dev'
+__version__ = '2.15.10'
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import sys
 import distutils.dir_util
 
 NAME = 'sdss-tree'
-VERSION = '2.15.10dev'
+VERSION = '2.15.10'
 RELEASE = 'dev' in VERSION
 
 
