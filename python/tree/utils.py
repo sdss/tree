2d306780ad0c2113deb8151f370c1be2ef38a38f
Brian Cherinka <havok2063@hotmail.com>
2020-04-26 12:23:29 -0400

    removing old changelog code

--- python/tree/utils.py
+++ python/tree/utils.py
@@ -17,187 +17,6 @@ import six
 from tree import Tree
 
 
-def compute2_changelog(new, old, pprint=None, remove_sas=True, include_paths=True):
-    ''' Compute the difference between two Tree environments
-
-    Finds and prints the difference between two tree environment
-    configurations.  Accepts either string names of config files, e.g.
-    "dr16" and "dr15", or the preloaded `Tree` configs, e.g.
-    `Tree(config='dr16')`.
-
-    Parameters:
-        new (str|Tree):
-            The new tree enviroment to compare
-        old (str|Tree):
-            The old tree environment to compare
-        pprint (bool):
-            If True, returns a single joined string for printing
-        remove_sas (bool):
-            If True, removes the SAS_BASE_DIR from environment values.  Default is True.
-        include_paths (bool):
-            If True, includes changes to the sdss_access PATHS section.  Default is True.
-
-    Returns:
-        A list of strings with printed changes
-
-    Example:
-        >>> # print the differences DR16 and DR15
-        >>> diffs = compute_changelog('dr16', 'dr15', pprint=True)
-        >>> print(diffs)
-    '''
-
-    if isinstance(new, six.string_types):
-        new = Tree(config=new.lower())
-
-    if isinstance(old, six.string_types):
-        old = Tree(config=old.lower())
-
-    new_envs = new.environ.keys()
-    old_envs = old.environ.keys()
-    new_name = new.environ['default']['name']
-    old_name = old.environ['default']['name']
-    sas = os.getenv("SAS_BASE_DIR")
-
-    diffs = ['Changes: {0} from {1}'.format(new_name.upper(), old_name.upper())]
-    diffs.append(' ')
-
-    # find unique new enviroments
-    unique_new_envs = set(new_envs) - set(old_envs)
-    for env in unique_new_envs:
-        diffs.append('New Environment: {0}'.format(env.upper()))
-        diffs.append('----------------------')
-        for k, v in new.environ[env].items():
-            if remove_sas:
-                v = v.split(sas)[-1]
-            diffs.append('{0}: {1}'.format(k.upper(), v))
-        diffs.append(' ')
-
-    # find unique old enviroments
-    unique_old_envs = set(old_envs) - set(new_envs)
-    for env in unique_old_envs:
-        diffs.append('Removed Environment: {0}'.format(env.upper()))
-        diffs.append('----------------------')
-        for k, v in old.environ[env].items():
-            if remove_sas:
-                v = v.split(sas)[-1]
-            diffs.append('{0}: {1}'.format(k.upper(), v))
-        diffs.append(' ')
-
-    # find new or updated environment variables
-    for env in new_envs:
-        # skip the default
-        if env == 'default' or env not in old.environ:
-            continue
-        uniq_vars = set(new.environ[env].keys()) - set(old.environ[env].keys())
-        if not uniq_vars:
-            continue
-
-        vbase = [v.split(os.path.join(sas, new_name))[-1] for v in new.environ[env].values()]
-        voldbase = [v.split(os.path.join(sas, old_name))[-1] for v in old.environ[env].values()]
-        changes = set(vbase) - set(voldbase)
-        if not changes:
-            continue
-
-        diffs.append('Changes in {0}:'.format(env.upper()))
-        diffs.append('----------------------')
-        for k, v in new.environ[env].items():
-            vold = old.environ[env].get(k, None)
-            if vold:
-                vbase = v.split(os.path.join(sas, new_name))[-1]
-                voldbase = vold.split(os.path.join(sas, old_name))[-1]
-                if vbase != voldbase:
-                    diffs.append('Updated {0}: {1} from {2}'.format(k.upper(), vbase, voldbase))
-            else:
-                if remove_sas:
-                    v = v.split(sas)[-1]
-                diffs.append('New {0}: {1}'.format(k.upper(), v))
-        diffs.append(' ')
-
-    if include_paths:
-        diffs.append('Changes in PATHS:')
-        diffs.append('-----------------')
-        path_changes = compute_path_changes(new, old)
-        diffs.extend(path_changes)
-
-    if pprint:
-        diffs = '\n'.join(diffs)
-    return diffs
-
-
-def compute_path_changes(new, old, pprint=None, prepend_header=None):
-    ''' Compute the difference between two Tree PATH sections
-
-    Finds and prints the difference between two tree PATH ini sections from
-    the given environment configurations.  Accepts either string names of config
-    files, e.g. "dr16" and "dr15", or the preloaded `Tree` configs, e.g.
-    `Tree(config='dr16')`.  Indicates new `sdss_access` paths, as well as any changes
-    to existing paths, between the two prodived releases.
-
-    Parameters:
-        new (str|Tree):
-            The new tree enviroment to compare
-        old (str|Tree):
-            The old tree environment to compare
-        pprint (bool):
-            If True, returns a single joined string for printing
-        prepend_header (bool):
-            If True, adds a header to the diff with release versions
-
-    Returns:
-        A list of strings with printed changes
-
-    Example:
-        >>> # print the differences DR16 and DR15
-        >>> diffs = compute_path_changes('dr16', 'dr15', pprint=True)
-        >>> print(diffs)
-    '''
-
-    if isinstance(new, six.string_types):
-        new = Tree(config=new.lower())
-
-    if isinstance(old, six.string_types):
-        old = Tree(config=old.lower())
-
-    new_templates = new.paths
-    old_templates = old.paths
-
-    diffs = []
-    if prepend_header:
-        new_name = new.environ['default']['name']
-        old_name = old.environ['default']['name']
-        diffs = ['Changes: {0} from {1}'.format(new_name.upper(), old_name.upper())]
-        diffs.append(' ')
-
-    unique_new = sorted(set(new_templates) - set(old_templates))
-
-    if unique_new:
-        diffs.append('New Paths:')
-        diffs.append('----------')
-        for path in unique_new:
-            diffs.append('{0}: {1}'.format(path, new_templates[path]))
-
-    # changes
-    changed = []
-    for name, template in new_templates.items():
-        if name in old_templates and template != old_templates[name]:
-            changed.append((name, template, old_templates[name]))
-
-    if changed:
-        diffs.append(' ')
-        diffs.append('Updated Paths:')
-        diffs.append('--------------')
-        for item in changed:
-            name, newt, oldt = item
-            diffs.append('{0}: '.format(name))
-            diffs.append('   from: {0}'.format(oldt))
-            diffs.append('   to: {0}'.format(newt))
-
-    if pprint:
-        diffs = '\n'.join(diffs)
-
-    return diffs
-
-
 def compute_changelog(new, old, pprint=None, to_list=None, remove_sas=True, include_paths=True):
     ''' Compute the difference between two Tree environments
 
@@ -234,11 +53,11 @@ def compute_changelog(new, old, pprint=None, to_list=None, remove_sas=True, incl
     '''
 
     # compute the environment changelog dictionary
-    cl_dict = _compute_environment(new, old, remove_sas=remove_sas)
+    cl_dict = compute_environment_changes(new, old, remove_sas=remove_sas)
 
     # compute and add the paths changelog dictionary
     if include_paths:
-        path_dict = _compute_paths(new, old)
+        path_dict = compute_path_changes(new, old)
         cl_dict.update(path_dict)
 
     if pprint or to_list:
@@ -247,7 +66,7 @@ def compute_changelog(new, old, pprint=None, to_list=None, remove_sas=True, incl
     return cl_dict
 
 
-def _compute_environment(new, old, remove_sas=True):
+def compute_environment_changes(new, old, remove_sas=True):
     ''' Compute the difference between two Tree environments
 
     Compares two tree environment configurations and returns a
@@ -334,7 +153,7 @@ def _compute_environment(new, old, remove_sas=True):
     return dd
 
 
-def _compute_paths(new, old, prepend_header=None):
+def compute_path_changes(new, old, prepend_header=None):
     ''' Compute the difference between two Tree PATH sections
 
     Compares two tree PATH ini sections from the given environment
