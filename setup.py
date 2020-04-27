35346f5633e2654e89084288c68694a36541d71d
Brian Cherinka <havok2063@hotmail.com>
2020-04-27 18:15:27 -0400

    testing setup cfg

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@
 #
 
 
+import distutils.dir_util
 from setuptools import setup
+
+# Copy data files into the python/tree/data directory
+tmp = distutils.dir_util.copy_tree('data', 'python/tree/data')
+
 
 setup()
 
